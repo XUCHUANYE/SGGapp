0349dae38db50e269eaa3d9b4812c12982ca5e82
XUCHUANYE <278789562@qq.com>
2022-11-19 19:12:34 +0800

    home组件页面完成

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import typenav from './views/Home/TypeNav/typenav.vue'
 
 Vue.config.productionTip = false
+
+Vue.component(typenav.name,typenav)
 
 new Vue({
   router,
--- src/views/Home/home.vue
+++ src/views/Home/home.vue
@@ -1,16 +1,40 @@
 <template>
   <div>
-    我是主页
+   <typenav/>
+   <listcontainer></listcontainer>
+   <recommed></recommed>
+   <rank></rank>
+   <like></like>
+   <firfoor></firfoor>
+   <secfloor></secfloor>
+   <brand></brand>
   </div>
 </template>
 
+
+
 <script>
+import listcontainer from '@/views/Home/ListContainer/listcontainer.vue';
+import recommed from '@/views/Home/Recommed/recommed.vue';
+import rank from '@/views/Home/Rank/rank.vue';
+import like from '@/views/Home/Like/like.vue';
+import firfoor from '@/views/Home/FirFoor/firfoor.vue';
+import secfloor from '@/views/Home/SecFloor/secfloor.vue';
+import brand from '@/views/Home/Brand/brand.vue';
 export default {
   name: "home",
   data() {
     return {};
   },
-  components: {},
+  components: {
+    listcontainer,
+    recommed,
+    rank,
+    like,
+    firfoor,
+    secfloor,
+    brand
+  },
 };
 </script>
 
